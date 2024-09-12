23d2a4c3666ff7dfc98e38810dee018d8fe5b2a3
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-09-12 05:22:39 +0500

    getting into backend

---
+++ app/api/[id]/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export function GET(request, response){
+    return NextResponse.json({
+        result: "success",
+        id: response.params.id
+    });
+};
